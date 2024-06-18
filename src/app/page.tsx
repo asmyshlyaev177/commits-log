7daef37f3da6930f9aa7399dc004efad901cdaad
asmyshlyaev177 <asmyshlyaev177@gmail.com>
2024-06-18 13:10:08 +0400

    fetchCommits to separate file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
-import { Octokit } from 'octokit';
-
-import packageJson from '@/../package.json';
-
-const token = process.env.GITHUB_TOKEN;
-
-const octokit = new Octokit({ auth: token });
+import { fetchCommits, type Commit } from './fetchCommits';
 
 export default async function Home() {
   const resp = await fetchCommits();
@@ -37,23 +31,4 @@ const Commit = ({ commit }: { commit: Commit }) => {
       )}
     </pre>
   );
-};
-
-type Commit = Awaited<ReturnType<typeof fetchCommits>>[number];
-
-// TODO: to separate file
-const fetchCommits = async () => {
-  const resp = await octokit.request('GET /repos/{owner}/{repo}/commits', {
-    owner: packageJson.author.name,
-    repo: packageJson.name,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28',
-    },
-  });
-
-  if (resp.status !== 200) {
-    return [];
-  }
-
-  return resp.data;
 };
